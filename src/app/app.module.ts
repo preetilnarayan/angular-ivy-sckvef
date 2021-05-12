ce8aa8a8246a128cef18dfcffdb16cd7b0c00d5a
preetilnarayan <preetilnarayan@gmail.com>
2021-05-12 19:05:10 +0530

    4 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, HeroesComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
